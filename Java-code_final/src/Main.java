44a1ef0a36bb0a362c7ab0f28a82c100355c0d2b
Mohamed Abusurra <mohamed.abusurra1@gmail.com>
2025-10-22 16:04:37 +0200

    feat: menu works correctly calling search with correct heuristic logic

--- Java-code_final/src/Main.java
+++ Java-code_final/src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
 
         while (true) {
-            System.out.println("1.solve with greedy best first search method");
+            System.out.println("1.solve with greedy first search method");
             System.out.println("2.solve with A* Search method");
             System.out.println("3.exit");
             int searchMethodChosen = scanner.nextInt();
@@ -33,23 +33,22 @@ public class Main {
             System.out.println("2. Solve with heuristic two");
             int heuristicChosen = scanner.nextInt();
 
-            if(heuristicChosen == 1)
-                //fill in logic later
-                System.out.println("a");
-             else if(heuristicChosen == 2)
-                 //fill in logic later//
-                System.out.println("a");
-             else
-                System.out.println("invalid number entered");
-
-            if (searchMethodChosen == 1)
-                System.out.println("a");
-            else
-                // call a star later with correct h chosen implementation later
-                System.out.println("b");
+            if(heuristicChosen != 1 && heuristicChosen != 2){
+                System.out.println("invalid number for heuristic choice entered");
+                continue;
+            }
 
 
+            if (searchMethodChosen == 1){
+                System.out.println("Greedy first search method with h" + heuristicChosen + ":");
+                tilePuzzle.greedyFirstSearch(start, heuristicChosen);
+            }
 
+            if (searchMethodChosen == 2){
+                System.out.println("A* search method with h" + heuristicChosen + ":");
+                tilePuzzle.greedyFirstSearch(start, heuristicChosen);
+            }
+            
         }
 
     }
