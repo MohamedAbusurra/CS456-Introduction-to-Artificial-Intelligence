c4c2711d47feaf98987fe1e0773dc8e3c31963c8
Mohamed Abusurra <mohamed.abusurra1@gmail.com>
2025-10-22 20:47:20 +0200

    docs: add inline comments explaining how code works

--- Java-code_final/src/TilePuzzle.java
+++ Java-code_final/src/TilePuzzle.java
@@ -13,6 +13,7 @@ public class TilePuzzle {
             {6, 7, 8}
     };
 
+    //Store key information required in the search in a node class that will be initalized
     private class Node {
         int[][] state;
         Node parent;
@@ -30,6 +31,9 @@ public class TilePuzzle {
             this.f_n = f_n;
         }
 
+        /*Overrided equals and hashcode so when two nodes are compared or node is checked to
+        see if contained by its configuration (arraylist,hashmap) in a data structure in GFS and A* does not compare
+         objects but 2d array of board configuration */
         @Override
         public boolean equals(Object obj) {
             Node node = (Node) obj;
@@ -55,7 +59,7 @@ public class TilePuzzle {
 
 
     }
-
+    //loops around entire board state configuration with a counter adding how many misplaced
     private int h1(int[][] state) {
         int heuristic = 0;
         for (int i = 0; i < n; i++) {
@@ -67,6 +71,7 @@ public class TilePuzzle {
         return heuristic;
     }
 
+    // loops to see number of tiles not in correct row plus number of tiles not in correct column and returns sum
     private int h2(int[][] state) {
         int heuristic = 0;
         for (int i = 0; i < n; i++) {
@@ -91,6 +96,8 @@ public class TilePuzzle {
         return heuristic;
     }
 
+    /* Used as a cleaner implementation of which heuristic was chosen for the search with the heuristic being returned of given
+    state and */
     private int heuristic(int[][] state, int heuristicChoice) {
         if (heuristicChoice == 1)
             return h1(state);
@@ -107,6 +114,10 @@ public class TilePuzzle {
         return new int[]{-1, -1};
     }
 
+    /*copies 2d board tile configuration because 2d arrays are sent as by reference
+     automatically and for altering tile configuration for creating successor states
+     it does not want to alter the original
+     */
     private int[][] copyBoard(int[][] state) {
         int[][] copy = new int[n][n];
         for (int i = 0; i < n; i++)
@@ -115,6 +126,8 @@ public class TilePuzzle {
         return copy;
     }
 
+
+    //checks if current board configuration state is the goal state
     private boolean isGoal(int[][] state) {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
@@ -124,6 +137,9 @@ public class TilePuzzle {
     }
 
     private int[][] moveUp(int[][] state) {
+        /* gets blank tile i and j position and alters it in a copy board and returns
+        new board configuration with blank tile moved up if possible
+         */
         int[][] copy = copyBoard(state);
         int[] blank = findBlank(copy);
         int positionI = blank[0];
@@ -134,11 +150,16 @@ public class TilePuzzle {
             copy[positionI - 1][positionJ] = temp;
             return copy;
         }
-
+        /*if null is returned, move up is not possible and condition operators use null comparison to ensure frontier
+        is appended incorrectly
+         */
         return null;
     }
 
     private int[][] moveDown(int[][] state) {
+        /* gets blank tile i and j position and alters it in a copy board and returns
+        new board configuration with blank tile moved down if possible
+         */
         int[][] copy = copyBoard(state);
         int[] blank = findBlank(copy);
         int positionI = blank[0];
@@ -154,6 +175,9 @@ public class TilePuzzle {
     }
 
     private int[][] moveLeft(int[][] state) {
+        /* gets blank tile i and j position and alters it in a copy board and returns
+        new board configuration with blank tile moved left if possible
+         */
         int[][] copy = copyBoard(state);
         int[] blank = findBlank(copy);
         int positionI = blank[0];
@@ -169,6 +193,9 @@ public class TilePuzzle {
     }
 
     private int[][] moveRight(int[][] state) {
+        /* gets blank tile i and j position and alters it in a copy board and returns
+        new board configuration with blank tile moved right if possible
+         */
         int[][] copy = copyBoard(state);
         int[] blank = findBlank(copy);
         int positionI = blank[0];
@@ -184,6 +211,7 @@ public class TilePuzzle {
     }
 
     private int lowestFrontierGreedyIndex(ArrayList<Node> frontier) {
+        //traverses frontier arrayList and returns index of board in frontier with the lowest heuristic function (h(n))
         int lowestHeuristicIndex = 0;
         for (int i = 1; i < frontier.size(); i++) {
             if (frontier.get(i).h_n < frontier.get(lowestHeuristicIndex).h_n)
@@ -193,6 +221,7 @@ public class TilePuzzle {
     }
 
     private int lowestAStarIndex(ArrayList<Node> frontier) {
+        //traverses frontier arrayList and returns index of board in frontier with the lowest function of n (f(n))
         int lowestFunctionIndex = 0;
         for (int i = 1; i < frontier.size(); i++) {
             if (frontier.get(i).f_n < frontier.get(lowestFunctionIndex).f_n)
@@ -203,6 +232,11 @@ public class TilePuzzle {
 
 
     private ArrayList<Node> successors(Node node, int heuristicChoice) {
+        /* takes board configuration (from node taken by parameter) and heuristic choose
+         and performs all possible successor actions calculating their h(n) (for heuristic chosen), g(n),
+          and f(n) and returning arraylist of nodes that are possible successor actions for node sent in
+          parameter
+         */
         ArrayList<Node> successorNodes = new ArrayList<Node>();
         int h_nNext;
         int g_nNext = node.g_n + 1;
@@ -237,6 +271,10 @@ public class TilePuzzle {
     }
 
     private void printSolution(Node goalNode, int enteredNodesCounter, int expandedNodesCounter) {
+        /* prints solution of goal Node with number of nodes which entered the frontier and
+        number of nodes which expanded from the frontier during the search and with manual changing
+        of boolean values printSteps and printPath, the path and/or the steps for the search will also be displayed
+         */
         ArrayList<Node> path = new ArrayList<Node>();
 
         for (Node node = goalNode; node != null; node = node.parent)
@@ -281,10 +319,11 @@ public class TilePuzzle {
     public void greedyFirstSearch(int[][] start, int heuristicChoice) {
         ArrayList<Node> frontier = new ArrayList<>();
         HashSet<Node> expanded = new HashSet<>();
+        //counter for nodes entered and expanded initialized
         int enteredNodesCounter = 0;
         int expandedNodesCounter = 0;
 
-
+        //Initializes frontier with start state node
         int h_n = heuristic(start, heuristicChoice);
         frontier.add(new Node(copyBoard(start), null, null, 0, h_n, h_n));
         enteredNodesCounter++;
@@ -318,9 +357,15 @@ public class TilePuzzle {
     public void aStarSearch(int[][] start, int heuristicChoice) {
         ArrayList<Node> frontier = new ArrayList<>();
         HashMap<Node, Integer> lowestSeenF_n = new HashMap<>();
+        //counter for nodes entered and expanded initialized
         int enteredNodesCounter = 0;
         int expandedNodesCounter = 0;
 
+        /*Initializes frontier with start state node and adds the node to a hashmap
+        with the function of n (f(n)) to be used when searching as a way to only append
+        input board configurations already in the frontier that have a lower f(n) than the one
+        currently in the frontier
+         */
         int h_n = heuristic(start, heuristicChoice);
         Node startNode = new Node(copyBoard(start), null, null, 0, h_n, h_n);
         frontier.add(startNode);
@@ -328,6 +373,7 @@ public class TilePuzzle {
         enteredNodesCounter++;
 
         while (!frontier.isEmpty()) {
+            // lowest f(n) board configuration is expanded and checked to see if goal state
             int index = lowestAStarIndex(frontier);
             Node node = frontier.remove(index);
             expandedNodesCounter++;
@@ -336,13 +382,19 @@ public class TilePuzzle {
                 printSolution(node,enteredNodesCounter, expandedNodesCounter);
                 return;
             }
+
+            /*finds all possible successor action nodes which are stored in an
+            array list and the array list is traversed seeing if current board
+            configuration exists (if not node configuration is added to frontier) and
+            if it exists checks to see which one of the two node board configurations
+            has a lower f(n). keeps the lower f(n) and discards the other
+             */
             ArrayList<Node> successorNodes = successors(node, heuristicChoice);
             for (int i = 0; i < successorNodes.size(); i++) {
                 Node successorNode = successorNodes.get(i);
 
                 Integer lowestF_n = lowestSeenF_n.get(successorNode);
 
-                // never seen this board -> record and enqueue
                 if (lowestF_n == null) {
                     lowestSeenF_n.put(successorNode, successorNode.f_n);
                     frontier.add(successorNode);
